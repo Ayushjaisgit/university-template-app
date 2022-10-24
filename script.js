58f5f08fa96501a0fb1e172cce0e9f31b4264002
Ayushjaisgit <ayushcodez@gmail.com>
2022-10-25 01:13:02 +0530

    Added main files

---
+++ script.js
@@ -0,0 +1,9 @@
+// <!-- --------------------javascript for togglemenu------------------->
+var navLinks = document.getElementById("navLinks")
+
+            function showmenu(){
+                navLinks.style.right ="0";
+            }
+            function hidemenu(){
+                navLinks.style.right ="-200px";
+            }
